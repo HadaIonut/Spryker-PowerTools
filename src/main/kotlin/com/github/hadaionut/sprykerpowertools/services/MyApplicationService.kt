b239032d1ab61a7b8f9b632d45c408dde10963da
GitHub Action <action@github.com>
2021-11-26 07:32:48 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hadaionut/sprykerpowertools/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hadaionut.sprykerpowertools.services
+
+import com.github.hadaionut.sprykerpowertools.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
